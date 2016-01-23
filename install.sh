4e197c21551e62de03530b4ff94c187e020c7dda
SamuelWN <samuelwn@gmail.com>
2016-01-23 00:24:53 +0000

    Added some sudo configuration

--- install.sh
+++ install.sh
@@ -2,26 +2,30 @@
 # Basic installation script for Snoopy NG requirements
 # glenn@sensepost.com // @glennzw
 # Todo: Make this an egg.
-set -e
 
+if [ $EUID != 0 ]; then
+   sudo "$0" "$@";
+fi
+
+set -e
 # In case this is the seconds time user runs setup, remove prior symlinks:
-rm -f /usr/bin/sslstrip_snoopy
-rm -f /usr/bin/snoopy
-rm -f /usr/bin/snoopy_auth
-rm -f /etc/transforms
+sudo rm -f /usr/bin/sslstrip_snoopy
+sudo rm -f /usr/bin/snoopy
+sudo rm -f /usr/bin/snoopy_auth
+sudo rm -f /etc/transforms
 
 echo "[+] Updating repository..."
-apt-get update
+sudo apt-get update
 
-apt-get install --force-yes --yes ntpdate
+sudo apt-get install --force-yes --yes ntpdate
 
 #if ps aux | grep ntp | grep -qv grep; then 
 if [ -f /etc/init.d/ntp ]; then
-	/etc/init.d/ntp stop
-else 
-	# Needed for Kali Linux build on Raspberry Pi
-	apt-get install ntp
-	/etc/init.d/ntp stop
+   /etc/init.d/ntp stop
+else
+   # Needed for Kali Linux build on Raspberry Pi
+   sudo apt-get install ntp
+   sudo /etc/init.d/ntp stop
 fi
 echo "[+] Setting time with ntp"
 ntpdate ntp.ubuntu.com 
@@ -29,60 +33,60 @@ ntpdate ntp.ubuntu.com
 
 echo "[+] Setting timzeone..."
 echo "Etc/UTC" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
+sudo dpkg-reconfigure -f noninteractive tzdata
 echo "[+] Installing sakis3g..."
-cp ./includes/sakis3g /usr/local/bin
+sudo cp ./includes/sakis3g /usr/local/bin
 
 # Packages
 echo "[+] Installing required packages..."
-apt-get install --force-yes --yes python-pip python-libpcap python-setuptools autossh python-psutil python2.7-dev libpcap0.8-dev ppp tcpdump python-serial sqlite3 python-requests iw build-essential python-bluez python-flask python-gps python-dateutil python-dev libxml2-dev libxslt-dev pyrit mitmproxy
+sudo apt-get install --force-yes --yes python-pip python-libpcap python-setuptools autossh python-psutil python2.7-dev libpcap0.8-dev ppp tcpdump python-serial sqlite3 python-requests iw build-essential python-bluez python-flask python-gps python-dateutil python-dev libxml2-dev libxslt-dev pyrit mitmproxy
 
 # Python packages
 
-easy_install pip
-easy_install smspdu
+sudo easy_install pip
+sudo easy_install smspdu
 
-pip install sqlalchemy==0.7.4
-pip uninstall requests -y
-pip install -Iv https://pypi.python.org/packages/source/r/requests/requests-0.14.2.tar.gz   #Wigle API built on old version
-pip install httplib2
-pip install BeautifulSoup
-pip install publicsuffix
-#pip install mitmproxy
-pip install pyinotify
-pip install netifaces
-pip install dnslib
+sudo pip install sqlalchemy==0.7.4
+sudo pip uninstall requests -y
+sudo pip install -Iv https://pypi.python.org/packages/source/r/requests/requests-0.14.2.tar.gz   #Wigle API built on old version
+sudo pip install httplib2
+sudo pip install BeautifulSoup
+sudo pip install publicsuffix
+#sudo pip install mitmproxy
+sudo pip install pyinotify
+sudo pip install netifaces
+sudo pip install dnslib
 
 #Install SP sslstrip
-cp -r ./setup/sslstripSnoopy/ /usr/share/
-ln -s /usr/share/sslstripSnoopy/sslstrip.py /usr/bin/sslstrip_snoopy
+sudo cp -r ./setup/sslstripSnoopy/ /usr/share/
+sudo ln -s /usr/share/sslstripSnoopy/sslstrip.py /usr/bin/sslstrip_snoopy
 
 # Download & Installs
 echo "[+] Installing pyserial 2.6"
-pip install https://pypi.python.org/packages/source/p/pyserial/pyserial-2.6.tar.gz
+sudo pip install https://pypi.python.org/packages/source/p/pyserial/pyserial-2.6.tar.gz
 
 echo "[+] Downloading pylibpcap..."
-pip install https://sourceforge.net/projects/pylibpcap/files/latest/download?source=files#egg=pylibpcap
+sudo pip install https://sourceforge.net/projects/pylibpcap/files/latest/download?source=files#egg=pylibpcap
 
 echo "[+] Downloading dpkt..."
-pip install https://dpkt.googlecode.com/files/dpkt-1.8.tar.gz
+sudo pip install https://dpkt.googlecode.com/files/dpkt-1.8.tar.gz
 
 echo "[+] Installing patched version of scapy..."
-pip install ./setup/scapy-latest-snoopy_patch.tar.gz
+sudo pip install ./setup/scapy-latest-snoopy_patch.tar.gz
 
 # Only run this on your client, not server:
-read -r -p  "[ ] Do you want to download, compile, and install aircrack? [y/n] " response
+read -r -p  "[?] Do you want to download, compile, and install aircrack? [y/n] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
    echo "[+] Installing required packages..."
-   apt-egt --force-yes --yes subversion libssl-dev libnl-genl-3-dev ethtool rfkill
+   sudo apt-get --force-yes --yes subversion libssl-dev libnl-genl-3-dev ethtool rfkill
    echo "[+] Downloading aircrack-ng..."
    svn co http://svn.aircrack-ng.org/trunk/ aircrack-ng   tar xzf aircrack-ng-1.2-beta1.tar.gz
    cd aircrack-ng
    echo "[-] Making aircrack-ng"
    make
    echo "[-] Installing aircrack-ng"
-   make install
+   sudo make install
    cd ../
    rm -rf aircrack-ng
 fi
@@ -91,12 +95,12 @@ echo "[+] Creating symlinks to this folder for snoopy.py."
 
 echo "sqlite:///`pwd`/snoopy.db" > ./transforms/db_path.conf
 
-ln -s `pwd`/transforms /etc/transforms
-ln -s `pwd`/snoopy.py /usr/bin/snoopy
-ln -s `pwd`/includes/auth_handler.py /usr/bin/snoopy_auth
-chmod +x /usr/bin/snoopy
-chmod +x /usr/bin/snoopy_auth
-chmod +x /usr/bin/sslstrip_snoopy
+sudo ln -s `pwd`/transforms /etc/transforms
+sudo ln -s `pwd`/snoopy.py /usr/bin/snoopy
+sudo ln -s `pwd`/includes/auth_handler.py /usr/bin/snoopy_auth
+sudo chmod +x /usr/bin/snoopy
+sudo chmod +x /usr/bin/snoopy_auth
+sudo chmod +x /usr/bin/sslstrip_snoopy
 
 echo "[+] Done. Try run 'snoopy' or 'snoopy_auth'"
 echo "[I] Ensure you set your ./transforms/db_path.conf path correctly when using Maltego"
