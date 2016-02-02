24742527b582c5bdd141ccbc9e8bfe8c2d38e685
SamuelWN <samuelwn@gmail.com>
2016-02-02 09:15:55 -0500

    Added removal of pre-installed version of python module scapy

--- install.sh
+++ install.sh
@@ -79,6 +79,9 @@ pip install ./setup/scapy-latest-snoopy_patch.tar.gz
 read -r -p  "[?] Do you want to download, compile, and install aircrack? [y/n] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
+   echo "[-] Removing default version of scapy..."
+   apt-get remove -y --force-yes python-scapy
+   pip uninstall scapy
    echo "[+] Installing required packages..."
    apt-get install --force-yes --yes subversion libssl-dev libnl-genl-3-dev ethtool pkg-config rfkill
    echo "[+] Downloading aircrack-ng..."
