16726297222796fdecbdd6db2e85f7bd9cd7c890
Samuel Walters-Nevet <SamuelWN@users.noreply.github.com>
2016-02-04 17:37:53 -0500

    Update start_snooping.sh
    Minor fix

--- start_snooping.sh
+++ start_snooping.sh
@@ -4,6 +4,8 @@
 # Run with command:
 #    nohup bash ./start_snoopy.sh > /dev/null &
 
+mkdir -p /tmp/Snoopy;
+
 alias LOCATION='Bedroom'
 IFACE=`ifconfig -a | sed 's/[ \t].*//;/^$/d' | grep mon`;
 
