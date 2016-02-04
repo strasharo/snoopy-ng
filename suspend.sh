ce33ac4ac5673ccff894a00c904ca4ad2160f445
Samuel Walters-Nevet <SamuelWN@users.noreply.github.com>
2016-02-04 17:41:38 -0500

    Update suspend.sh

--- suspend.sh
+++ suspend.sh
@@ -6,9 +6,9 @@
 
 at now + 10 hours -f ./startup.sh
 
-SERVER=<server address for database storage>
-NETWORK=<WiFi network to use for database upload>
-DATABASE="~/snoopy-ng/snoopy.db"
+#SERVER=<server address for database storage>
+#NETWORK=<WiFi network to use for database upload>
+#DATABASE="~/snoopy-ng/snoopy.db"
 NODE=`whoami`           # Username should be in the form of "NODE1", "NODE2", etc
 
 sudo kill /tmp/Snoopy/*.pid
@@ -20,24 +20,24 @@ IFACE=`ifconfig -a | sed 's/[ \t].*//;/^$/d' | grep wlan`;
 sudo ifconfig $IFACE up;
 
 sudo iwconfig $IFACE mode managed;
-sudo iwconfig $IFACE essid "${NETWORK}";
+#sudo iwconfig $IFACE essid "${NETWORK}";
 sudo dhclient $IFACE;
 
 COUNTER=1;
 
-while [  $COUNTER -lt 4 ]; do
-    if [ scp -P 7900 $DATABASE woodstock@"${SERVER}":/some/remote/directory -eq 0 ]; then
-        rm $DATABASE;
-        let COUNTER=10;
-    else
-        sleep 30;
-        let COUNTER=COUNTER+1;
-    fi
-done
+#while [  $COUNTER -lt 4 ]; do
+#    if [ scp -P 7900 $DATABASE woodstock@"${SERVER}":/some/remote/directory -eq 0 ]; then
+#        rm $DATABASE;
+#        let COUNTER=10;
+#    else
+#        sleep 30;
+#       let COUNTER=COUNTER+1;
+#    fi
+#done
 
-if [ $COUNTER -eq 10 ]; then
-    echo "Database synced successfully." tee -a ./Database.log
-else
+#if [ $COUNTER -eq 10 ]; then
+#    echo "Database synced successfully." tee -a ./Database.log
+#else
     echo "Database failed to sync. Data will be maintained locally." tee -a ./Database.log
 
 sudo ifconfig $IFACE down;
