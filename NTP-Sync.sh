76aa1d7e7edc076dc8546732beaa1b8d13decd4f
SamuelWN <samuelwn@gmail.com>
2016-02-18 14:17:25 -0500

    Short-circuit NTP-Sync (temporary measure) Define variables used by WiFi-Connect Other minor changes

--- NTP-Sync.sh
+++ NTP-Sync.sh
@@ -1,8 +1,12 @@
-$(cat /etc/SNOOP_DIR.conf)/Wifi-Connect.sh
+#!/bin/bash
+#
+# This script starts Snoopy
 
-if [ -f /etc/init.d/ntp ]; then
-   /etc/init.d/ntp stop
-else
+# Need to ensure Wifi-Connect works properly before proceeding
+exit 0;
+
+
+$(cat /etc/SNOOP_DIR.conf)/WiFi-Connect.sh
 
 echo "[+] Setting time with ntp"
 ntpdate ntp.ubuntu.com
--- WiFi-Connect.sh
+++ WiFi-Connect.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+#
+# Connects the to the WiFi network "$NETWORK"
+#   (designed for use with an unsecured network)
+
 NETWORK="<WiFi network to use for database upload>"
+IFACE=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | grep wlan);
 
 sudo ifconfig $IFACE up;
 sudo iwconfig $IFACE mode managed;
-sudo iwconfig $IFACE essid "${NETWORK}";
+sudo iwconfig $IFACE essid "${NETWORK}" key open;
 sudo dhclient $IFACE;
 
