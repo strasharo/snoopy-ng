5d91381c399292d057ed54686fcf30b5be8c9cdb
SamuelWN <samuelwn@gmail.com>
2016-03-17 13:41:50 -0400

    undo error

--- install.sh
+++ install.sh
@@ -3,14 +3,6 @@
 # glenn@sensepost.com // @glennzw
 # Todo: Make this an egg.
 
-<<<<<<< HEAD
-if [ $EUID != 0 ]; then {
-   echo "Please run this script as root.";
-   exit;
-fi
-
-set -e
-=======
 if [ $# -gt 0 ] && [[ $1 =~ ^([-]*[h?][eE]?[lL]?[pP]?) ]]; then
   echo " ___  _  _  _____  _____  ____  _  _"
   echo "/ __)( \( )(  _  )(  _  )(  _ \( \/ )"
@@ -96,7 +88,6 @@ fi
 
 set -e
 
->>>>>>> ef28e16a820cba20d90b2e3bfc52c4896e57b90f
 # In case this is the seconds time user runs setup, remove prior symlinks:
 rm -f /usr/bin/sslstrip_snoopy
 rm -f /usr/bin/snoopy
@@ -108,11 +99,7 @@ apt-get update
 
 apt-get install --force-yes --yes ntpdate
 
-<<<<<<< HEAD
-#if ps aux | grep ntp | grep -qv grep; then 
-=======
 #if ps aux | grep ntp | grep -qv grep; then
->>>>>>> ef28e16a820cba20d90b2e3bfc52c4896e57b90f
 if [ -f /etc/init.d/ntp ]; then
    /etc/init.d/ntp stop
 else
@@ -121,36 +108,21 @@ else
    /etc/init.d/ntp stop
 fi
 echo "[+] Setting time with ntp"
-<<<<<<< HEAD
-ntpdate ntp.ubuntu.com 
-/etc/init.d/ntp start
-
-echo "[+] Setting timzeone..."
-echo "Etc/UTC" > /etc/timezone
-=======
 ntpdate ntp.ubuntu.com
 /etc/init.d/ntp start
 
 echo "[+] Setting timzeone..."
 echo "America/New_York" > /etc/timezone
->>>>>>> ef28e16a820cba20d90b2e3bfc52c4896e57b90f
 dpkg-reconfigure -f noninteractive tzdata
 echo "[+] Installing sakis3g..."
 cp ./includes/sakis3g /usr/local/bin
 
 # Packages
 echo "[+] Installing required packages..."
-<<<<<<< HEAD
-apt-get install --force-yes --yes python-pip python-libpcap python-setuptools autossh python-psutil python2.7-dev libpcap0.8-dev ppp tcpdump python-serial sqlite3 python-requests iw build-essential python-bluez python-flask python-gps python-dateutil python-dev libxml2-dev libxslt-dev pyrit mitmproxy
-
-# Python packages
-
-=======
 apt-get install --force-yes --yes python-pip python-libpcap python-setuptools autossh python-psutil python2.7-dev libpcap0.8-dev ppp at tcpdump \
   python-serial sqlite3 python-requests iw build-essential python-bluez python-flask python-gps python-dateutil python-dev libxml2-dev libxslt-dev pyrit mitmproxy
 
 # Python packages
->>>>>>> ef28e16a820cba20d90b2e3bfc52c4896e57b90f
 easy_install pip
 easy_install smspdu
 
@@ -177,9 +149,6 @@ echo "[+] Downloading pylibpcap..."
 pip install https://sourceforge.net/projects/pylibpcap/files/latest/download?source=files#egg=pylibpcap
 
 echo "[+] Downloading dpkt..."
-<<<<<<< HEAD
-pip install https://dpkt.googlecode.com/files/dpkt-1.8.tar.gz
-=======
 pip install dpkt
 
 echo "[-] Removing default version of scapy..."
@@ -187,43 +156,11 @@ apt-get remove -y --force-yes python-scapy
 if ! [ -z "$(pip list | grep "scapy")" ]; then
   pip uninstall -y -q scapy;
 fi
->>>>>>> ef28e16a820cba20d90b2e3bfc52c4896e57b90f
 
 echo "[+] Installing patched version of scapy..."
 pip install ./setup/scapy-latest-snoopy_patch.tar.gz
 
 # Only run this on your client, not server:
-<<<<<<< HEAD
-read -r -p  "[?] Do you want to download, compile, and install aircrack? [y/n] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-   echo "[+] Installing required packages..."
-   apt-get install --force-yes --yes subversion libssl-dev libnl-genl-3-dev ethtool pkg-config rfkill
-   echo "[+] Downloading aircrack-ng..."
-   svn co http://svn.aircrack-ng.org/trunk/ aircrack-ng
-   cd aircrack-ng
-   echo "[-] Making aircrack-ng"
-   make
-   echo "[-] Installing aircrack-ng"
-   make install
-   cd ../
-   rm -rf aircrack-ng
-fi
-
-echo "[+] Creating symlinks to this folder for snoopy.py."
-
-echo "sqlite:///`pwd`/snoopy.db" > ./transforms/db_path.conf
-
-ln -s `pwd`/transforms /etc/transforms
-ln -s `pwd`/snoopy.py /usr/bin/snoopy
-ln -s `pwd`/includes/auth_handler.py /usr/bin/snoopy_auth
-chmod +x /usr/bin/snoopy
-chmod +x /usr/bin/snoopy_auth
-chmod +x /usr/bin/sslstrip_snoopy
-
-echo "[+] Done. Try run 'snoopy' or 'snoopy_auth'"
-echo "[I] Ensure you set your ./transforms/db_path.conf path correctly when using Maltego"
-=======
 if [ $# -ne 0 ]; then
   if [[ $1 == "-c" ]]; then
       response="yes";
@@ -295,4 +232,3 @@ cd $RET_DIR
 #   This is only intended for use in part of a class project.
 #   Please ccomment out the following line unless you are are already intricately familiar with this software and its liscencing policies:
 echo "Accepted" > ${SNOOP_DIR}/.acceptedlicense
->>>>>>> ef28e16a820cba20d90b2e3bfc52c4896e57b90f
