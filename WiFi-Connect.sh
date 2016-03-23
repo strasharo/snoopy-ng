bf55335cd1964353717319b0abefc40f3504af87
SamuelWN <samuelwn@gmail.com>
2016-03-23 07:48:20 -0400

    WiFi managed through ifup/ifdown now

--- WiFi-Connect.sh
+++ WiFi-Connect.sh
@@ -3,11 +3,12 @@
 # Connects the to the WiFi network "$NETWORK"
 #   (designed for use with an unsecured network)
 
-NETWORK="<WiFi network to use for database upload>"
+# NETWORK="<WiFi network to use for database upload>"
 IFACE=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | grep wlan);
 
-sudo ifconfig $IFACE up;
-sudo iwconfig $IFACE mode managed;
-sudo iwconfig $IFACE essid "${NETWORK}" key open;
+# sudo ifconfig $IFACE up;
+# sudo iwconfig $IFACE mode managed;
+# sudo iwconfig $IFACE essid "${NETWORK}" key open;
+sudo ifup $IFACE;
 sudo dhclient $IFACE;
 
