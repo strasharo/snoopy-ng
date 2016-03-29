02ccd7c4110d307adbea99393150efe5a82db1e3
SamuelWN <samuelwn@gmail.com>
2016-03-28 22:04:27 -0400

    Minor changes

--- scripts/rc_local.sh
+++ scripts/rc_local.sh
@@ -4,11 +4,21 @@
 # Get current time with NTP:
 bash $(cat /etc/SNOOP_DIR.conf)/NTP-Sync.sh
 
-# Send
-echo "date +%F' '%T
-Remote: $(dig +short myip.opendns.com @resolver1.opendns.com)
-Local: $(ifconfig $IFACE | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
-" | ssh "${SERVER}" "cat > /home/${USER}/${LOCATION}/${DEVICE}/IP.log"
+# Send IP address info to remote server
+if [ -f /etc/SNOOP_DIR.conf ]; then
+    SNOOP_DIR=$(cat /etc/SNOOP_DIR.conf)
+    if [ -f ${SNOOP_DIR}/.server ] && [ -f ${SNOOP_DIR}/.DeviceName ] && [ -f ${SNOOP_DIR}/.DeviceLoc ]
+    then
+        SERVER=`cat ${SNOOP_DIR}/.server`
+        DEVICE=`cat ${SNOOP_DIR}/.DeviceName`
+        LOCATION=`cat ${SNOOP_DIR}/.DeviceLoc`
+
+        echo "date +%F' '%T
+        Remote: $(dig +short myip.opendns.com @resolver1.opendns.com)
+        Local: $(ifconfig $IFACE | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
+        " | ssh "${SERVER}" "cat > /home/${USER}/${LOCATION}/${DEVICE}/IP.log"
+    fi
+fi
 
 # Runs Snoopy:
 bash $(cat /etc/SNOOP_DIR.conf)/startup.sh
