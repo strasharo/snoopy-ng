fc4621f3e96b1305148ac9ae1b72550c4407a123
SamuelWN <samuelwn@gmail.com>
2016-03-28 21:40:32 -0400

    Sync IP

--- scripts/rc_local.sh
+++ scripts/rc_local.sh
@@ -4,5 +4,12 @@
 # Get current time with NTP:
 bash $(cat /etc/SNOOP_DIR.conf)/NTP-Sync.sh
 
+# Send
+echo "date +%F' '%T
+Remote: $(dig +short myip.opendns.com @resolver1.opendns.com)
+Local: $(ifconfig $IFACE | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
+" | ssh "${SERVER}" "cat > /home/${USER}/${LOCATION}/${DEVICE}/IP.log"
+
 # Runs Snoopy:
 bash $(cat /etc/SNOOP_DIR.conf)/startup.sh
+
--- sync_db.sh
+++ sync_db.sh
@@ -43,12 +43,12 @@ if [ -f "$DATABASE" ]; then
     while [ $COUNTER -lt 4 ]; do
         if [ scp $DATABASE "${SERVER}:/home/${USER}/${LOCATION}/${DEVICE}" -eq 0 ]; then
 
-            IPs="
+            IPs="`date +%F' '%T`
 Remote: $(dig +short myip.opendns.com @resolver1.opendns.com)
 Local: $(ifconfig $IFACE | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
 "
 
-            echo "${IPs}" | ssh "${SERVER}" 'cat >> /home/"${USER}"/"${LOCATION}"/"${DEVICE}"/IP.log'
+            echo "${IPs}" | ssh "${SERVER}" "cat > /home/${USER}/${LOCATION}/${DEVICE}/IP.log"
 
             let COUNTER=10;
         else
