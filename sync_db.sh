2e4aa45426e3304a0735e7f06045ba4cd999017e
SamuelWN <samuelwn@gmail.com>
2016-04-01 12:27:55 -0400

    Debug

--- sync_db.sh
+++ sync_db.sh
@@ -50,7 +50,7 @@ if [ -f "$DATABASE" ]; then
     ssh -F /home/pi/.ssh/config "${SERVER}" mkdir -p "/home/snoopy/${LOCATION}/${DEVICE}/"
 
     while [ $COUNTER -lt 4 ]; do
-        scp  -F /home/pi/.ssh/config $DATABASE "${SERVER}:/home/${USER}/${LOCATION}/${DEVICE}"
+        scp  -F /home/pi/.ssh/config "$DATABASE" "${SERVER}:/home/${USER}/${LOCATION}/${DEVICE}"
         if [ $? -eq 0 ]; then
 
             IPs="`date +%F' '%T`
