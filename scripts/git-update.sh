9e8f3164a607905c5f39e23f2ceed0452c7a1e77
SamuelWN <samuelwn@gmail.com>
2016-04-02 14:30:39 -0400

    Debug

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -1,10 +1,10 @@
 cd $(cat /etc/SNOOP_DIR.conf)
 
 # Downloads the latest files from remote (without trying to merge, rebase, etc)
-su "git fetch --all" pi
+su -c "git fetch --all" pi
 
 # Resets folder to state of files in branch `origin/master` branch
 # Note: Files not tracked by git (e.g. ".DevicenName", ".DeviceLoc", etc) are unaffected
-su "git reset --hard origin/master" pi
-f
+su -c "git reset --hard origin/master" pi
+
 # PS: git commands are run as user 'pi' to avoid any unforeseen issues that may arise from making all files owned by the super user.
